79c9dce078d08dc366f09661d620f7020915f400
Nikolay Kostov <admin@nikolay.it>
2014-10-20 19:37:56 +0300

    Added demonstration of explicit constructors used in combination with primary constructor

---
+++ 2. PrimaryConstructors/3. PrimaryConstructorsWithExplicitConstructors.cs
@@ -0,0 +1,18 @@
+﻿namespace _2.PrimaryConstructors
+{
+    public class PersonWithFewConstructors(string firstName, string lastName)
+    {
+        public PersonWithFewConstructors()
+            : this("Nikolay", "Kostov") // Without this line the code will not compile
+        {
+        }
+
+        public PersonWithFewConstructors(int age)
+            : this() // This will call indirectly the primary constructor
+        {
+        }
+
+        public string FirstName { get; set; } = firstName;
+        public string LastName { get; set; } = lastName;
+    }
+}
