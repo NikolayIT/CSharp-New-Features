8f39be45a137ab80c22d66e846b42331a966b655
Nikolay Kostov <admin@nikolay.it>
2014-10-20 19:05:44 +0300

    Added demonstration for primary constructors with bodies

---
+++ 2. PrimaryConstructors/2. PrimaryConstructorBodies.cs
@@ -0,0 +1,22 @@
+﻿namespace _2.PrimaryConstructors
+{
+    using System;
+
+    public class PersonWithPrimaryConstructorBody(string firstName, string lastName)
+    {
+        {
+            if (firstName == null)
+            {
+                throw new ArgumentNullException("firstName");
+            }
+
+            if (lastName == null)
+            {
+                throw new ArgumentNullException("lastName");
+            }
+        }
+
+        public string FirstName { get; } = firstName;
+        public string LastName { get; } = lastName;
+    }
+}
--- 2. PrimaryConstructors/Program.cs
+++ 2. PrimaryConstructors/Program.cs
@@ -8,6 +8,12 @@
         {
             var person = new Person("Nikolay", "Kostov");
             Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
+
+            var personWithPrimaryConstructorBody
+                = new PersonWithPrimaryConstructorBody("Nikolay", "Kostov"); // try with null
+            Console.WriteLine("{0} {1}",
+                personWithPrimaryConstructorBody.FirstName,
+                personWithPrimaryConstructorBody.LastName);
         }
     }
 }
