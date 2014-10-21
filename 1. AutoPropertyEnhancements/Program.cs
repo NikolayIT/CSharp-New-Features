9f9cf717a33d9334d4ccf6491463815188a4e7dc
Nikolay Kostov <admin@nikolay.it>
2014-10-21 16:34:22 +0300

    Improvements in some demonstrations

--- 1. AutoPropertyEnhancements/Program.cs
+++ 1. AutoPropertyEnhancements/Program.cs
@@ -6,9 +6,11 @@
     {
         internal static void Main()
         {
+            // Initializers for auto-properties
             var person = new Person();
             Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
 
+            // Getter-only auto-properties
             var readOnlyPerson = new ReadOnlyPerson();
             Console.WriteLine("{0} {1}", readOnlyPerson.FirstName, readOnlyPerson.LastName);
         }
--- 4. UsingStatic/Program.cs
+++ 4. UsingStatic/Program.cs
@@ -1,13 +1,20 @@
 ﻿namespace _4.UsingStatic
 {
+    using System.Linq;
+
     using System.Console;
     using System.Math;
 
-    class Program
+    internal class Program
     {
-        static void Main()
+        internal static void Main()
         {
+            // Using static
             WriteLine(Pow(2, 10));
+
+            // Using static in combination with extension methods
+            var oddNumbersGenerator = new UsingExtensionMethodsStaticClass();
+            oddNumbersGenerator.GetOddNumbersBetween(1, 10).ToList().ForEach(WriteLine);
         }
     }
 }
