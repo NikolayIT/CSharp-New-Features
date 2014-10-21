785180f0a4dc6e36046786cf0465805e40add212
Nikolay Kostov <admin@nikolay.it>
2014-10-21 19:08:29 +0300

    Added demonstration for the nameof() operator

---
+++ 7. NameofExpressions/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace _7.NameofExpressions
+{
+    using System;
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            if (firstName == null)
+            {
+                throw new ArgumentNullException(nameof(firstName));
+            }
+
+            if (lastName == null)
+            {
+                throw new ArgumentNullException(nameof(lastName));
+            }
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0} {1}", this.firstName, this.lastName);
+            }
+        }
+    }
+}
---
+++ 7. NameofExpressions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7.NameofExpressions
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            try
+            {
+                var person = new Person(null, null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
