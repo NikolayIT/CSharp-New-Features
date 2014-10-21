44a66f5ad41e7ee5b783f71cd1379d3e85da917f
Nikolay Kostov <admin@nikolay.it>
2014-10-21 18:38:10 +0300

    Added demonstrations for declaration expressions (inline variable declaration) This feature is suspended (it will not be present in the next official version) but still it can be used in the experimental version of the compiler

---
+++ 6. DeclarationExpressions/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _6.DeclarationExpressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        internal static void Main()
+        {
+            // Read number and print the power 2 of the number
+            if (int.TryParse(var line = Console.ReadLine(), out int i))
+            {
+                Console.WriteLine("Result: {0}", (var x = i) * x, x);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input: {0}", line);
+            }
+
+            // Convert list of strings to list of integers
+            var strings = new List<string> { "123", "-5", "1337", "3777" };
+            var numbers = from s in strings select int.TryParse(s, out int i) ? i : -1;
+        }
+    }
+}
