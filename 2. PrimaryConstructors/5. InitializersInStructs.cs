5268197ac3529c393595e8184ea8c27ad7ec7d3d
Nikolay Kostov <admin@nikolay.it>
2014-10-21 18:01:08 +0300

    Added demonstration for initializers in structs

---
+++ 2. PrimaryConstructors/5. InitializersInStructs.cs
@@ -0,0 +1,20 @@
+﻿namespace _2.PrimaryConstructors
+{
+    public struct Tally(int count, int sum)
+    {
+        public int Count { get; } = count;
+        public int Sum { get; } = sum;
+        public double Average
+        {
+            get
+            {
+                if (Count == 0)
+                {
+                    return 0;
+                }
+
+                return (double)Sum / Count;
+            }
+        }
+    }
+}
