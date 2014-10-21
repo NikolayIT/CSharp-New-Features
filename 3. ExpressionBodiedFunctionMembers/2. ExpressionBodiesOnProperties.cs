0407246c5f4eef23023d90beb628c66288c9428b
Nikolay Kostov <admin@nikolay.it>
2014-10-21 14:07:19 +0300

    Added demonstration for using expressions as property getters

---
+++ 3. ExpressionBodiedFunctionMembers/2. ExpressionBodiesOnProperties.cs
@@ -0,0 +1,32 @@
+﻿namespace _3.ExpressionBodiedFunctionMembers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Person
+    {
+        public Person()
+        {
+            this.Children = new List<Person>();
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            this.Children = new List<Person>();
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public List<Person> Children { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Name => string.Format("{0} {1}", FirstName, LastName);
+
+		public Person this[string name] =>
+			this.Children.FirstOrDefault(
+				x => x.Name.ToLower().Contains(name.ToLower()));
+    }
+}
--- 3. ExpressionBodiedFunctionMembers/Program.cs
+++ 3. ExpressionBodiedFunctionMembers/Program.cs
@@ -9,13 +9,22 @@
         internal static void Main()
         {
             // 1. Expression bodies on method-like members
+            Console.WriteLine("1. Expression bodies on method-like members");
             PrintTime();
             var point = new Point(37, 77);
             var point2 = point.Clone() as Point;
             point.Move(100, 100);
             point2.Print();
+            Console.WriteLine();
 
             // 2. Expression bodies on property-like function members
+            Console.WriteLine("2. Expression bodies on property-like function members");
+            var person = new Person("John", "Smith");
+            person.Children.Add(new Person("Junior 1", "Smith"));
+            person.Children.Add(new Person("Junior 2", "Smith"));
+            person.Children.Add(new Person("Junior 3", "Smith"));
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person["Junior 2 Smith"].Name);
         }
     }
 }
