0ef105cbd9706e9af9006fe5b268628ed8367f8c
Nikolay Kostov <admin@nikolay.it>
2014-10-21 11:45:20 +0300

    Added few missing files

---
+++ 3. ExpressionBodiedFunctionMembers/1. ExpressionBodiesOnMethods.cs
@@ -0,0 +1,27 @@
+﻿namespace _3.ExpressionBodiedFunctionMembers
+{
+    using System;
+
+    public class Point : ICloneable
+    {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public void Print() => Console.WriteLine(this.X + " " + this.Y);
+
+        public object Clone() => new Point(this.X, this.Y);
+
+        public void Move(int x, int y)
+        {
+            this.X += x;
+            this.Y += y;
+        }
+    }
+}
---
+++ 3. ExpressionBodiedFunctionMembers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _3.ExpressionBodiedFunctionMembers
+{
+    using System;
+
+    internal class Program
+    {
+        public static void PrintTime() => Console.WriteLine("Current time: {0}", DateTime.Now.ToString());
+
+        internal static void Main()
+        {
+            // 1. Expression bodies on method-like members
+            PrintTime();
+            var point = new Point(37, 77);
+            var point2 = point.Clone() as Point;
+            point.Move(100, 100);
+            point2.Print();
+
+            // 2. Expression bodies on property-like function members
+        }
+    }
+}
