36bcd7da85978e53098d0c22e04c750c49352ac1
Nikolay Kostov <admin@nikolay.it>
2014-10-21 16:34:47 +0300

    Added demonstration for exception filters

---
+++ 5. ExceptionFilters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5.ExceptionFilters
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            Person person = null;
+            try
+            {
+                string firstName = "Nikolay";
+                string lastName = null;
+                person = new Person(firstName, lastName);
+            }
+            catch (ArgumentNullException ex) if (ex.ParamName == "firstName")
+            {
+                Console.WriteLine("First name is null");
+            }
+            catch (ArgumentNullException ex) if (ex.ParamName == "lastName")
+            {
+                Console.WriteLine("Last name is null");
+            }
+
+            if (person != null)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+    }
+}
