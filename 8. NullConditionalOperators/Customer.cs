29fa96d8ca32d86052f5a035d41c253f851d91df
Nikolay Kostov <admin@nikolay.it>
2014-10-22 11:34:03 +0300

    Added demonstrations of Null-conditional operators

---
+++ 8. NullConditionalOperators/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace _8.NullConditionalOperators
+{
+    using System.Collections.Generic;
+
+    public class Customer
+    {
+        public Customer()
+        {
+            this.Orders = new List<string>();
+        }
+
+        public IEnumerable<string> Orders { get; set; }
+    }
+}
---
+++ 8. NullConditionalOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _8.NullConditionalOperators
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        internal static void Main()
+        {
+            Customer[] customers = null;
+            // Customer[] customers = new Customer[1] { new Customer() { Orders = new List<string> { "order1", "order2" } } };
+
+            int? lengthOrNull = customers?.Length; // null if customers is null
+            Console.WriteLine("lengthOrNull.HasValue: {0}", lengthOrNull.HasValue);
+
+            Customer first = customers?[0]; // null if customers is null
+
+            int lengthOrZero = customers?.Length ?? 0; // 0 if customers null
+            Console.WriteLine("lengthOrZero: {0}", lengthOrZero);
+
+            int? firstOrdersCount = customers?[0].Orders.Count();
+            Console.WriteLine("firstOrdersCount.HasValue: {0}", firstOrdersCount.HasValue);
+            Console.WriteLine("firstOrdersCount: {0}", firstOrdersCount);
+        }
+    }
+}
