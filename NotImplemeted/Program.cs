3f26d5c2b9c307348d1d2ea4e4365a0c870c66ba
Nikolay Kostov <admin@nikolay.it>
2014-10-23 16:43:35 +0300

    Added 2 not implemented C# 6.0 features (http://roslyn.codeplex.com/wikipage?title=Language%20Feature%20Status&referringTitle=Documentation)

---
+++ NotImplemeted/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NotImplemeted
+{
+    // Private protected modifier
+    /*
+    public class Test
+    {
+        private protected int Age { get; set; }
+    }
+    */
+
+    internal class Program
+    {
+        internal static void Main()
+        {
+            /*
+            // Binary literals and digit separators
+            var bits = 0b00101110;
+            var hex = 0x00_2E;
+            var dec = 1_234_567_890;
+            */
+        }
+    }
+}
