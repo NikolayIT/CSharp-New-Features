034fcbec4e7e1cdc1e0bc03f99f1ec27344b0585
Nikolay Kostov <admin@nikolay.it>
2014-10-23 17:14:54 +0300

    Added two not implemented features "Indexed member initializer" and "Indexed member access"

--- NotImplemeted/Program.cs
+++ NotImplemeted/Program.cs
@@ -1,7 +1,8 @@
 ﻿namespace NotImplemeted
 {
-    // Private protected modifier
-    /*
+    using System;
+
+    /* // Private protected modifier
     public class Test
     {
         private protected int Age { get; set; }
@@ -12,11 +13,25 @@
     {
         internal static void Main()
         {
-            /*
-            // Binary literals and digit separators
+            /* // Binary literals and digit separators
             var bits = 0b00101110;
             var hex = 0x00_2E;
             var dec = 1_234_567_890;
+            */
+
+            /* // Indexed member initializer
+            var obj = new JObject { $x = 3, $y = 7 };
+            // Equivalent to
+            var obj = new JObject();
+            obj["first"] = "Nikolay";
+            obj["last"] = "Kostov";
+            */
+
+            /* // Indexed member access
+            obj.$name = obj.$first + " " + obj.$last;
+            // Equivalent to
+            obj["name"] = obj["first"] + " " + obj["last"];
+            Console.WriteLine(obj["name"]);
             */
         }
     }
