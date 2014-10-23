1359ad38a96a0a8aa2ddcd67b5206d90f57d72ce
Nikolay Kostov <admin@nikolay.it>
2014-10-23 18:40:30 +0300

    Added demonstration for await keyword in catch/finally blocks

---
+++ 10. AwaitInCatchFinally/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _10.AwaitInCatchFinally
+{
+    using System;
+    using System.IO;
+
+    internal class Program
+    {
+        internal static void Main()
+        {
+            ReadData(@"..\..\Program.cs", @"Log.txt");
+            Console.ReadLine();
+        }
+
+        private static async void ReadData(string fileName, string logFileName)
+        {
+            var input = new StreamReader(fileName);
+            var log = new StreamWriter(logFileName);
+            try
+            {
+                var line = await input.ReadLineAsync();
+                Console.WriteLine("Line written");
+            }
+            catch (IOException ex)
+            {
+                await log.WriteLineAsync(ex.ToString());
+                Console.WriteLine("Log written");
+            }
+            finally
+            {
+                if (log != null) await log.FlushAsync();
+                Console.WriteLine("Log flushed");
+            }
+        }
+    }
+}
