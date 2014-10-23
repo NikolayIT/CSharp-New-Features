86c46d93ecafbb635a0f3c168084399f796d0a49
Nikolay Kostov <admin@nikolay.it>
2014-10-23 15:35:09 +0300

    Added demonstration for index initializers

---
+++ 9. IndexInitializers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _9.IndexInitializers
+{
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        internal static void Main()
+        {
+            // C# 5.0
+            var numbersOldWay = new Dictionary<int, string>
+            {
+               { 7, "seven" },
+               { 9, "nine" },
+               { 13, "thirteen" }
+            };
+
+            // C# 6.0
+            var numbers = new Dictionary<int, string>
+            {
+                [7] = "seven",
+                [9] = "nine",
+                [13] = "thirteen"
+            };
+
+            // Same as
+            // numbers = new Dictionary<int, string>();
+            // numbers[7] = "seven";
+            // numbers[9] = "nine";
+            // numbers[13] = "thirteen";
+
+            // Another example
+            var dictionary = new Dictionary<string, string>
+            {
+                ["C#"] = "A programming language",
+                [".NET"] = "A development platform",
+            };
+        }
+    }
+}
