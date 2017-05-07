133e0fd8c9f2ca1b0b2d0b8fe63e9ba0f608ba3e
Nikolay Kostov <admin@nikolay.it>
2017-05-07 21:07:08 +0300

    Example for C# 7.0 "More Expression-bodied Members" added

---
+++ CSharp-7.0-New-Features/01. MoreExpressionBodiedMembers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        using (var resource = new Resource())
+        {
+            resource.ToStringCalledEvent += (sender, args)
+                => Console.WriteLine("ToString() called event handled.");
+            resource.X = 1;
+            Console.WriteLine(resource);
+        }
+
+        Console.WriteLine("Resource is no longer needed.");
+    }
+}
---
+++ CSharp-7.0-New-Features/01. MoreExpressionBodiedMembers/Resource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Resource : IDisposable
+{
+    private int x;
+    private EventHandler toStringCalledEventHandler;
+
+    // C# 7.0 - Constructor
+    public Resource() => Console.WriteLine($"Constructing {nameof(Resource)}...");
+
+    // C# 7.0 - Destructor
+    ~Resource() => Console.WriteLine("Destructing...");
+
+    // C# 7.0 - Event accessors
+    public event EventHandler ToStringCalledEvent
+    {
+        add => this.toStringCalledEventHandler += value;
+        remove => this.toStringCalledEventHandler -= value;
+    }
+
+    // C# 6.0 - Get-accessor only property
+    public int Y => 42;
+
+    // C# 7.0 - Property accessors
+    public int X
+    {
+        get => this.x;
+        set => this.x = value;
+    }
+
+    // C# 6.0 - Method
+    public void Dispose() => Console.WriteLine("Disposing...");
+
+    public override string ToString()
+    {
+        this.toStringCalledEventHandler(this, EventArgs.Empty);
+        return $"({this.X}, {this.Y})";
+    }
+}
