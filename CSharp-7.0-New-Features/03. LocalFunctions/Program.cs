a152ab250f775821f6977d6d2d704bc22b7ce4ae
Nikolay Kostov <admin@nikolay.it>
2017-05-08 12:25:41 +0300

    Added examples for C# 7.0 local functions

---
+++ CSharp-7.0-New-Features/03. LocalFunctions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(ConcatenateStrings("a", "b", "c", "d"));
+        Console.WriteLine(UsingClosure("a", "b", "c", "d"));
+    }
+
+    private static string UsingClosure(string a, string b, string c, string d)
+    {
+        string Concatenate()
+        {
+            return a + b + c + d;
+        }
+
+        return Concatenate();
+    }
+
+    public static string ConcatenateStrings(string a, string b, string c, string d)
+    {
+        void Validate(string paramName, object param)
+        {
+            if (param == null)
+            {
+                throw new ArgumentException($"{paramName} cannot be null", paramName);
+            }
+        }
+
+        Validate(nameof(a), a);
+        Validate(nameof(b), b);
+        Validate(nameof(c), c);
+        Validate(nameof(d), d);
+
+        return $"{a}{b}{c}{d}";
+    }
+}
