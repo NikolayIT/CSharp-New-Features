c98cb2e49851955715dcf4900665485b40c5cb8a
Nikolay Kostov <admin@nikolay.it>
2017-05-08 16:00:06 +0300

    Added example for throwing an exception in the middle of an expression

---
+++ CSharp-7.0-New-Features/07. ThrowExpressions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        PrintName("Nikolay");
+
+        var array = new[] { 42 };
+        var arrayFirstValue = (array.Length > 0)
+                                  ? array[0]
+                                  : throw new Exception($"{nameof(array)} contains no elements.");
+        Console.WriteLine(arrayFirstValue);
+    }
+
+    private static void PrintName(string name)
+    {
+        var firstName = name ?? throw new ArgumentNullException();
+        Console.WriteLine(firstName);
+    }
+
+    public static string DoMagic() => throw new NotImplementedException();
+}
