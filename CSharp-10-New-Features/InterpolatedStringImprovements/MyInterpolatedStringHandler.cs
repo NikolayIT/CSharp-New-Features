8c844a9e930ce4ef1570d3c55c63ea20f8b5e81a
Nikolay.IT <admin@nikolay.it>
2021-12-05 17:06:20 +0200

    Add InterpolatedStringHandlerExample in CSharp-10-New-Features

---
+++ CSharp-10-New-Features/InterpolatedStringImprovements/MyInterpolatedStringHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace InterpolatedStringImprovements
+{
+    [InterpolatedStringHandler]
+    public ref struct MyInterpolatedStringHandler
+    {
+        public MyInterpolatedStringHandler(int literalLength, int formattedCount)
+        {
+            Console.WriteLine($"MyInterpolatedStringHandler: literal length: {literalLength}, formattedCount: {formattedCount}");
+        }
+
+        public void AppendLiteral(string s)
+        {
+            Console.WriteLine($"AppendLiteral called: \"{s}\"");
+        }
+
+        public void AppendFormatted<T>(T t)
+        {
+            Console.WriteLine($"AppendFormatted called: \"{t}\" is of type {typeof(T).Name}");
+        }
+    }
+}
--- CSharp-10-New-Features/InterpolatedStringImprovements/Program.cs
+++ CSharp-10-New-Features/InterpolatedStringImprovements/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
+using System.Text;
 
-namespace InterpolatedStringImprovements 
+namespace InterpolatedStringImprovements
 {
     public static class Program
     {
+        // Constant interpolated strings
+        const string FirstName = "Niki", LastName = "Kostov";
+        const string FullName = $"{FirstName} {LastName}";
+
         public static void Main()
         {
             // Interpolated string handlers
+            var sb = new StringBuilder();
+            sb.Append($"Hello {Environment.UserName}, how are you?");
 
-            // Constant interpolated strings
+            // StringBuilder is using InterpolatedStingHandler
+            // to optimize performance and reduce string allocations
+            // The call above will be equivalent to:
+            sb.Append("Hello ");
+            sb.Append(Environment.UserName);
+            sb.Append(", how are you?");
 
+            InterpolatedStringHandlerExample($"Hello {Environment.UserName}, how are you?");
+        }
+
+        public static void InterpolatedStringHandlerExample(MyInterpolatedStringHandler handler)
+        {
         }
     }
 }
